a88c94dec1fe231cf1953083f8544d3cc4d072da
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-03-23 00:12:40 +0530

    create compones & pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Pages from './pages/Pages';
 
 function App() {
   return (
     <div className="App">
       <h1>
-        hello
+        <Pages/>
       </h1>
 
     </div>
