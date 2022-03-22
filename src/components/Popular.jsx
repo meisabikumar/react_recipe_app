3ca798d7feee9991de12c9d3c20d7937df64770d
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-03-23 00:32:50 +0530

    calling API and storing data useState, useEffect

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Popular() {
+  const [popular, setpopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+    const data = await api.json();
+
+    setpopular(data.recipes);
+    console.log(data);
+  };
+
   return (
-    <div>Popular</div>
-  )
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div>
+            <p> {recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Popular
+export default Popular;
