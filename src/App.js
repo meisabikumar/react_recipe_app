e357305698755339f2e26840fc07b288522ae8ac
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-03-25 02:09:25 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 // import './App.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import Category from "./components/Category";
 import Search from "./components/Search";
 import Pages from "./pages/Pages";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"} >
+            Deliciouss
+            </Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -15,5 +23,23 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
